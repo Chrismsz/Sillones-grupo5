c79791e07de9c63ef78b4386199430fdce93e236
Christopher Silva <Chris-OSX@MacBook-Pro-de-Christopher.local>
2020-07-26 23:01:23 -0400

    Subida Spring

---
+++ src/main/java/com/Grupo5/MCSSillones/McsSillonesApplication.java
@@ -0,0 +1,13 @@
+package com.Grupo5.MCSSillones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsSillonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsSillonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Grupo5/MCSSillones/McsSillonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo5.MCSSillones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsSillonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
